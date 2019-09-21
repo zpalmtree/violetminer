0f147a92b3fdbee50e15046af41a804f90314fad
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-21 22:40:47 +0100

    .

--- scripts/win-install.bat
+++ scripts/win-install.bat
@@ -1,6 +1,15 @@
 @echo off
 
-cmake -DENABLE_NVIDIA=OFF -G "Visual Studio 15 2017 Win64" .. || exit /b 1
+copy CUDAVisualStudioIntegration\\extras\\visual_studio_integration\\MSBuildExtensions\\* "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\v150\BuildCustomizations"
+
+set PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin\";%PATH%
+set CUDACXX="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin\nvcc"
+
+md build
+
+cd build
+
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release .. || exit /b 1
 
 MSBuild violetminer.sln /p:Configuration=Release /m || exit /b 1 
 
