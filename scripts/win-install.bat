c2bad62b781fdafb51ec497c6253d99ee6c871b6
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-22 00:13:35 +0100

    .

--- scripts/win-install.bat
+++ scripts/win-install.bat
@@ -3,10 +3,10 @@
 copy CUDAVisualStudioIntegration\\extras\\visual_studio_integration\\MSBuildExtensions\\* "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\BuildCustomizations\"
 
 set PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin\";%PATH%
-set CUDACXX="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin\nvcc"
-set CUDA_TOOLKIT_ROOT_DIR="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1"
-set CUDA_PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1"
-set CudaToolkitDir="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1"
+set CUDACXX=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin\nvcc
+set CUDA_TOOLKIT_ROOT_DIR=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1
+set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1
+set CudaToolkitDir=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1
 
 nvcc --version
 
