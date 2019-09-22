434c9ff09bbc6a00ab78acfeda70550f2193a7a1
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-22 05:10:56 +0100

    Cuda windows travis support

--- scripts/win-install.bat
+++ scripts/win-install.bat
@@ -1,8 +1,21 @@
-@echo off
+copy CUDAVisualStudioIntegration\\extras\\visual_studio_integration\\MSBuildExtensions\\* "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\BuildCustomizations\"
 
-cmake -DENABLE_NVIDIA=OFF -G "Visual Studio 15 2017 Win64" .. || exit /b 1
+set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin;%PATH%
 
-MSBuild violetminer.sln /p:Configuration=Release /m || exit /b 1 
+set CudaToolkitDir=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1
+
+echo %PATH%
+
+nvcc --version
+
+md build
+
+cd build
+
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VS_PLATFORM_TOOLSET_CUDA=10.1 .. || type CMakeFiles\CMakeError.log || type CMakeFiles\CMakeOutput.log || exit /b 1
+
+MSBuild violetminer.sln /p:Configuration=Release /m /v:n || exit /b 1 
 
 cd Release
 
