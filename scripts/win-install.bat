dadffe2c7becfb92e70c5ad040cc346f5933f88d
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-22 02:59:00 +0100

    .

--- scripts/win-install.bat
+++ scripts/win-install.bat
@@ -1,7 +1,7 @@
 copy CUDAVisualStudioIntegration\\extras\\visual_studio_integration\\MSBuildExtensions\\* "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\BuildCustomizations\"
 
 set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin;%PATH%
-set PATH=C:\Program Files (x86)/Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin;%PATH%
 
 :: set CUDACXX=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin\nvcc
 :: REM set CUDA_TOOLKIT_ROOT_DIR=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1
@@ -18,7 +18,7 @@ cd build
 
 cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VS_PLATFORM_TOOLSET_CUDA=10.1 .. || type CMakeFiles\CMakeError.log || type CMakeFiles\CMakeOutput.log || exit /b 1
 
-MSBuild violetminer.sln /p:Configuration=Release /m /v:d || exit /b 1 
+MSBuild violetminer.sln /p:Configuration=Release /m /v:n || exit /b 1 
 
 cd Release
 
