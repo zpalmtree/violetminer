c423c1b60def74ec9ae9eadb3dca056f8dfa0392
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-22 02:16:32 +0100

    .

--- scripts/win-install.bat
+++ scripts/win-install.bat
@@ -16,7 +16,7 @@ cd build
 
 cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VS_PLATFORM_TOOLSET_CUDA=10.1 .. || type CMakeFiles\CMakeError.log || type CMakeFiles\CMakeOutput.log || exit /b 1
 
-MSBuild violetminer.sln /p:Configuration=Release /m /v:diag || exit /b 1 
+MSBuild violetminer.sln /p:Configuration=Release /m /v:d || exit /b 1 
 
 cd Release
 
