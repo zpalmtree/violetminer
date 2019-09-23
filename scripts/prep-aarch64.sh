0d6cf0d4dd3eb8c2b326783bd4f4ef4af29d7441
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-09-24 00:32:35 +0100

    Silent download

--- scripts/prep-aarch64.sh
+++ scripts/prep-aarch64.sh
@@ -25,7 +25,7 @@ mkdir -p $TOOLCHAIN_DIR && cd $TOOLCHAIN_DIR
 echo -n "Checking for aarch64-linux-gnu-gcc... "
 if [ ! -f $TOOLCHAIN_DIR/gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc ]; then
   echo "Not found... Installing..."
-  wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/8.2-2018.08/gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu.tar.xz
+  wget -nv https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/8.2-2018.08/gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu.tar.xz
   tar xfv gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu.tar.xz >/dev/null
   echo "aarch64-linux-gnu-gcc: Installed"
 else
@@ -45,7 +45,7 @@ export CUSTOM_TOOLCHAIN_FILE=../scripts/cross-aarch64.cmake
 echo -n "Checking for OpenSSL 1.0.2r..."
 if [ ! -f $TOOLCHAIN_DIR/openssl/lib/libcrypto.a ]; then
   echo "Not found... Installing..."
-  wget https://www.openssl.org/source/openssl-1.0.2r.tar.gz
+  wget -nv https://www.openssl.org/source/openssl-1.0.2r.tar.gz
   tar zxvf openssl-1.0.2r.tar.gz >/dev/null
   cd openssl-1.0.2r
   ./Configure linux-aarch64 --prefix=$TOOLCHAIN_DIR/openssl --openssldir=$TOOLCHAIN_DIR/openssl >/dev/null
