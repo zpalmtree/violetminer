4701032b38f527cf4bd12fec5328c08ca7546826
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-11-05 11:20:43 +0000

    .

--- scripts/win-install.bat
+++ scripts/win-install.bat
@@ -1,13 +1,27 @@
 copy CUDAVisualStudioIntegration\\extras\\visual_studio_integration\\MSBuildExtensions\\* "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\BuildCustomizations\"
 
 dir C:\Program Files\NVIDIA Corporation
-echo "test"
-dir "C:\Program Files\NVIDIA Corporation"
 
-set PATH=C:\Program Files\NVIDIA Corporation\CUDA\v8.0\bin;%PATH%
+echo "test"
+
+dir "C:\Program Files\NVIDIA Corporation"
+dir "C:\Program Files"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin"
+
+dir C:\Program Files\NVIDIA Corporation
+dir C:\Program Files
+dir C:\Program Files\NVIDIA GPU Computing Toolkit
+dir C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA
+dir C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0
+dir C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin
+
+set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%PATH%
 set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin;%PATH%
 
-set CudaToolkitDir=C:\Program Files\NVIDIA Corporation\CUDA\v8.0
+set CudaToolkitDir=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0
 
 echo %PATH%
 
