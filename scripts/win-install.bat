7d86c9567f7545204e82712bbd1ddd3c054d015c
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-11-05 11:40:20 +0000

    .

--- scripts/win-install.bat
+++ scripts/win-install.bat
@@ -1,25 +1,9 @@
 copy CUDAVisualStudioIntegration\\extras\\visual_studio_integration\\MSBuildExtensions\\* "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\BuildCustomizations\"
 
-dir C:\Program Files\NVIDIA Corporation
-
-echo "test"
-
-dir "C:\Program Files\NVIDIA Corporation"
-dir "C:\Program Files"
-dir "C:\Program Files\NVIDIA GPU Computing Toolkit"
-dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
-dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0"
-dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin"
-
-dir C:\Program Files\NVIDIA Corporation
-dir C:\Program Files
-dir C:\Program Files\NVIDIA GPU Computing Toolkit
-dir C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA
-dir C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0
-dir C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin
-
 set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%PATH%
 set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\bin\x86_amd64;%PATH%
 
 set CudaToolkitDir=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0
 
@@ -33,7 +17,7 @@ cd build
 
 cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_VS_PLATFORM_TOOLSET_CUDA=8.0 .. || type CMakeFiles\CMakeError.log || type CMakeFiles\CMakeOutput.log || exit /b 1
 
-MSBuild violetminer.sln /p:Configuration=Release /m /v:n || exit /b 1 
+MSBuild violetminer.sln /p:Configuration=Release /m /v:d || exit /b 1 
 
 cd Release
 
